a9ad861ffdbef493c834bac56ff7f94859536e28
Sam Corcos <samcorcos@gmail.com>
2014-12-10 21:55:49 -0500

    refactoring grouped

--- client/views/graphs/grouped/grouped.js
+++ client/views/graphs/grouped/grouped.js
@@ -2,6 +2,10 @@ Template.grouped.rendered = function() {
   // jQuery tab initialization
   $('ul.tabs').tabs();
 
+  createGrouped();
+};
+
+var createGrouped = function() {
   var height = 600,
       width = 1000;
 
@@ -34,58 +38,51 @@ Template.grouped.rendered = function() {
 
   // This creates the force... Between the elements?
   var force = d3.layout.force()
-    .nodes(nodes)
-    .size([width, height])
-    .on("tick", tick)
-    .start();
+  .nodes(nodes)
+  .size([width, height])
+  .on("tick", tick)
+  .start();
 
-    // This creates the SVG on the DOM
+  // This creates the SVG on the DOM
   var svg = d3.select("#grouped-network")
-    .append("svg")
-    .attr({
-      height: height,
-      width: width
-    });
+  .append("svg")
+  .attr({
+    height: height,
+    width: width
+  });
 
-    // This
+  // This
   var node = svg.selectAll(".node")
-    .data(nodes)
+  .data(nodes)
   .enter().append("circle")
-    .attr("class", "node")
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
-    .attr("r", 8)
-    .style("fill", function(d,i) { return fill(i & 3); })
-    .style("stroke", function(d,i) { return d3.rgb(fill(i & 3)).darker(2); })
-    .call(force.drag)
-    .on("mousedown", function() { d3.event.stopPropagation(); })
+  .attr("class", "node")
+  .attr("cx", function(d) { return d.x; })
+  .attr("cy", function(d) { return d.y; })
+  .attr("r", 8)
+  .style("fill", function(d,i) { return fill(i & 3); })
+  .style("stroke", function(d,i) { return d3.rgb(fill(i & 3)).darker(2); })
+  .call(force.drag)
+  .on("mousedown", function() { d3.event.stopPropagation(); })
 
-    // This sets the opacity of the SVG so it transitions in.
+  // This sets the opacity of the SVG so it transitions in.
   svg.style("opacity", 1e-6)
-    .transition()
-      .duration(1000)
-      .style("opacity", 1);
+  .transition()
+  .duration(1000)
+  .style("opacity", 1);
 
-      // This selects the SVG and runs the "mousedown" function, which is the perturbation function, when clicked.
+  // This selects the SVG and runs the "mousedown" function, which is the perturbation function, when clicked.
   d3.select("#grouped-network")
-    .on("mousedown", mousedown);
+  .on("mousedown", mousedown);
 
 
-    var groups = { 'law': {x:500, y:450},
-                   'media': {x:400, y:250},
-                   'entrepreneur': {x:1000, y:0},
-                   'finance': {x:1000, y:600},
-                   'fish_monger': {x:0, y:0},
-                   'retail': {x:0, y:600},
-                   'technology': {x:500, y:50}}
+  var groups = { 'law': {x:500, y:450},
+  'media': {x:400, y:250},
+  'entrepreneur': {x:1000, y:0},
+  'finance': {x:1000, y:600},
+  'fish_monger': {x:0, y:0},
+  'retail': {x:0, y:600},
+  'technology': {x:500, y:50}}
   function tick(e) {
-
-    // This is where the clustering happens... I'm not sure how, but it is.
-    // var k = 6 * e.alpha; // "alpha" is the "cooling factor" that slows down the nodes over time. You'll notice that the nodes start the simulation moving quickly, then eventually settle into place. That's because of alpha (http://vallandingham.me/bubble_charts_in_d3.html).
-    // nodes.forEach(function(o,i) {
-    //   o.y += i & 1 ? k : -k;
-    //   o.x += i & 2 ? k : -k;
-    // });
 
     var k = e.alpha * .1;
     nodes.forEach(function(node) {
@@ -98,7 +95,7 @@ Template.grouped.rendered = function() {
 
 
     node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+    .attr("cy", function(d) { return d.y; });
   }
 
   // To disable perturbing, disable this function
@@ -109,8 +106,7 @@ Template.grouped.rendered = function() {
     });
     force.resume();
   }
-
-};
+}
 
 // I want the information to be perturbed when you click on a new tab.
 Template.grouped.events({
--- client/views/graphs/grouped/new_grouped.js
+++
@@ -1,9 +0,0 @@
-// Template.grouped.rendered = function () {
-//
-// }
-//
-// Template.grouped.events({
-//   "click .tab": function(event, template) {
-//
-//   }
-// })
